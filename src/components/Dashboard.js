f8d5eddc9d5cfde9a4a0c2db7f8da01f1676a738
unknown <akliu113@gmail.com>
2019-05-31 11:22:08 -0700

    created base dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Dashboard extends Component {
-  	render() {
-  		console.log(this.props)
-    	return (
-      		<div>
-        		Dashboard
-      		</div>
-    	)
-  	}
+	render() {
+  	return (
+  		<div>
+    		<h3 className='center'>Your Timeline</h3>
+        <ul className='dashboard-list'>
+          {this.props.tweetIds.map((id) => (
+            <li key={id}>
+              <div>TWEET ID: {id}</div>
+            </li>
+          ))}
+        </ul>
+  		</div>
+  	)
+	}
 }
 
+//the only thing we need from the store is the list of tweets
 function mapStateToProps( {tweets} ) {
 	return {
 		tweetIds: Object.keys(tweets)
 			.sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
 	}
 }
-export default Dashboard
+
+export default connect(mapStateToProps)(Dashboard)
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,6 +2,8 @@ import thunk from 'redux-thunk'
 import logger from './logger'
 import { applyMiddleware } from 'redux'
 
+//if you import "middleware" from the middleware folder,
+//this is what gets imported
 export default applyMiddleware(
 	thunk,
 	logger,
